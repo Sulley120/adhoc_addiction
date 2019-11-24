dd4d42745ed47536640fa4658fd1bce001043400
Tyrell <wagnert23@mymacewan.ca>
2019-11-24 14:13:25 -0700

    root sender/receiver

--- adhoc/app.cc
+++ adhoc/app.cc
@@ -1,14 +1,16 @@
 
 
-byte node_ID = 0;
-
-
+byte nodeID = 0;
+byte pathID;
+byte RSSI_C;
+byte LQI_C;
+byte hopCount;
 
 struct msg {
 	byte nodeID;
 	byte pathID;
 	byte hopCount;
-	byte powerLVL;
+	word powerLVL;
 }
 
 
@@ -23,7 +25,12 @@ msg node_init() {
 
 fsm root {
 
-	
+	struct msg * payload;
+	lword time;
+	address packet;
+	word p1, tr;
+	byte RSSI, LQI;
+	int count = 0;
 
 	state Init:
 
@@ -31,10 +38,81 @@ fsm root {
 		tcv_control (sfd, PHYSOPT_SETPOWER, &power);
 		tcv_control (sfd, PHYSOPT_GETPOWER, &ReadPower);
 			
-	
-	state:
+		
 
 	state Sending:
+
+		//Sets timer
+		time = seconds();
+		packet = tcv_wnp(sending, sfd, 9);
+		packet[0] = 0;
+
+		char * p (char *)(packet+1);
+		*p = payload->nodeID;p++;
+		*p = payload->pathID;p++;
+		*p = payload->hopCount;p++;
+		*p = payload->powerLVL;p++;
+
+		tcv_endp_(packet);
+		ufree(payload);
+
+	state Receive_Connection:
+
+		//RSSI is check by potential parents
+		packet = tcv_rnp(Receiving_Connection, sfd);
+		
+		//Checks timer
+		if((seconds()-time) > 90){
+			proceed Power_UP; 
+		}
+
+	state Measuring:
+
+		p1 = (tcv_left(packet))>>1;
+		tr = packet[p1-1];
+		RSSI = (byte) (tr>>8);
+		LQI = (byte) tr;
+
+	state Update:
+		//Parent is set to the node id of the message 
+		//So the node id will always be id of the node who
+		//sent the message
+		struct msg* payload = (struct msg*)(packet + 1);
+		
+		//checks for multiple connections
+		count ++;
+		if(count > 1){
+			leds_all(0);
+			led(3, 1);
+			// If distance is short look for another
+			// connection
+			if((payload->hopCount + 1) > hopCount){
+				proceed Receive_Connection:
+			}
+		}
+		//updates if better connection
+		RSSI_C = RSSI;
+		LQI_C = LQI;
+		hopCount = payload->hopCount + 1;
+		pathID = payload->nodeID;
+
+		tcv_endp(packet);
+		proceed Receive_Connection;
+
+	//increments power until max and if max sends to shutdown.
+	state Power_Up:
+		word power;
+		tcv_control (sfd, PHYSOPT_GETPOWER,(&power));
+		if(power == 7){
+			proceed Shut_Down;
+		}
+
+		tcv_control (sfd, PHYSOPT_SETPOWER,(&power + 1));
+		proceed Sending;	
+
+	state Shut_Down:
+		leds_all(0);
+		finish;
 
 }
 
