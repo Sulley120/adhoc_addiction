d3575215479e68f2a57b130c9d834dbbe72c128b
campbelld226 <campbelld226@mymacewan.ca>
2019-12-01 15:08:30 -0700

    change runfsm to call

--- adhoc/app.cc
+++ adhoc/app.cc
@@ -48,7 +48,7 @@ struct msg {
 };
 
 // Function to create and initialize a msg struct
-struct msg * msg_init(byte destID, byte sourceID, byte connect, byte hopCount, byte ReadPower) {
+struct msg * msg_init(byte destID, byte sourceID, byte connect, byte hopCount, word ReadPower) {
 	struct msg * node;
 	node = (struct msg *)umalloc(sizeof(struct msg));
 	node->nodeID = nodeID;
@@ -56,185 +56,20 @@ struct msg * msg_init(byte destID, byte sourceID, byte connect, byte hopCount, b
 	node->sourceID = sourceID;
 	node->connect = connect;
 	node->hopCount = hopCount;
-	node->powerLVL = ReadPower;
+	node->powerLVL = (byte) ReadPower;
 
 	return node;
-}
-
-// Main fsm
-fsm root {
-	struct msg * payload;
-	lword t;
-	lword timeout;
-	address packet;
-	word p1, tr;
-	byte RSSI, LQI;
-	byte ReadPower;
-	int count = 0;
-	char c;
-
-	/* initializes the root */
-	state Init:
-		phys_cc1100(0, CC1100_BUF_SZ);
-		tcv_plug(0, &plug_null);
-		sfd = tcv_open(NONE, 0, 0);
-		if (sfd < 0) {
-			diag("unable to open TCV session.");
-			syserror(EASSERT, "no session");
-		}
-		tcv_control(sfd, PHYSOPT_ON, NULL);
-
-	/* Initializes the msg packet */
-	state Init_t:
-		tcv_control (sfd, PHYSOPT_SETPOWER, &power);
-		tcv_control (sfd, PHYSOPT_GETPOWER, &ReadPower);		
-		leds(1, 1);
-
-	/* Ask if this node will be the sink */
-	state ASK_ser:
-		timeout = seconds()
-		ser_outf(ASK_ser, "Would you like to make this the sink node? (y/n)\n\r");
-
-	/* Wait for response */
-	state WAIT_ser:
-
-		if(((seconds())-timeout)>30){proceed Sending;}
-		ser_inf(WAIT_ser, "%c", &c);
-       	/* Sets the sink nodeID to be 0. */
-		if(c == 'y') {
-			nodeID = 0;
-			parentID = 0;
-			hopCount = 0;
-			// Run the receive fsm and don't try to connect to the tree
-			call receive;
-			proceed End;
-		}
-		//payload->nodeID = nodeID;
-
-	// Sends a request to join the tree if not sink node
-	state Sending:
-		payload = msg_init(-1, nodeID, 1, 0, ReadPower);
-		//Sets timer
-		t = seconds();
-		packet = tcv_wnp(Sending, sfd, 12);
-		packet[0] = 0;
-
-		char * p = (char *)(packet+1);
-		*p = payload->nodeID;p++;
-		*p = payload->destID;p++;
-		*p = payload->sourceID;p++;
-		*p = payload->connect;p++;
-		*p = payload->hopCount;p++;
-		*p = payload->powerLVL;
-
-		tcv_endp(packet);
-		ufree(payload);
-
-	// Wait to receive a response from a node in the tree
-	state Wait_Connection:
-		// At end of 1.5 seconds, check if the node received a connection
-		if((seconds()-t) > 1.5){
-			// If connection end
-			if(count >= 1){
-				call receive;
-				proceed Connected;
-			}
-			//else power up
-            proceed Power_Up;
-        }
-		// RSSI is checked by potential parents
-		packet = tcv_rnp(Receive_Connection, sfd);
-
-	// Get information from received packet
-	state Measuring:
-		p1 = (tcv_left(packet))>>1;
-		tr = packet[p1-1];
-		RSSI = (byte) (tr>>8);
-		LQI = (byte) tr;
-
-	// Compare the responding nodes and only save the best one
-	state Update:
-		struct msg* payload = (struct msg*)(packet + 1);
-		if (payload->connect == 1) {
-			// Checks for multiple responses
-			count ++;
-			if(count > 1){
-				leds_all(0);
-				leds(2, 1);
-				// If the newly received response is further away than a previous response
-				if((payload->hopCount + 1) > hopCount){
-					proceed Receive_Connection;
-				}
-			}
-			// Updates if better connection
-			RSSI_C = RSSI;
-			LQI_C = LQI;
-			hopCount = payload->hopCount + 1;
-			/* parentID is set to the nodeID of the message
-			So the node id will always be id of the node who
-			sent the message */
-			parentID = payload->nodeID;
-			nodeID = payload->destID;
-		}
-
-		tcv_endp(packet);
-		// Go back to listening for connections until 1.5 seconds passes
-		proceed Wait_Connection;
-
-	// Increments power until max and if max sends to shutdown.
-	state Power_Up:
-		if(power == 7){
-			proceed Shut_Down;
-		}
-		power++;
-		tcv_control (sfd, PHYSOPT_SETPOWER,(&power));
-		proceed Sending;
-
-	// If no suitable nodes respond, shut down.
-	state Shut_Down:
-		leds_all(0);
-		finish;
-
-	// Generates final connection message
-	state Prep_Message:
-		payload = (struct msg *)umalloc(sizeof(struct msg));
-		payload->nodeID = nodeID;
-       		payload->destID = destID;
-		payload->connect = 1;
-		payload->hopCount = hopCount;
-		payload->powerLVL = (byte) ReadPower;
-
-	// Informs parent that it now has a child
-	state Connected:
-		payload = msg_init(destID, nodeID, 1, hopCount, ReadPower);
-		packet = tcv_wnp(Connected, sfd, 12);
-		packet[0] = 0;
-
-		char * p = (char *)(packet+1);
-		*p = payload->nodeID;p++;
-		*p = payload->destID;p++;
-		*p = payload->sourceID;p++;
-		*p = payload->connect;p++;
-		*p = payload->hopCount;p++;
-		*p = payload->powerLVL;
-
-		tcv_endp(packet);
-		ufree(payload);
-		call receiving;
-		
-	// If root finishes the whole program stops. Keep root running.
-	// TODO: Is there a better way for root to continue infinitely? This will end eventually
-	state End:
-		delay(10000000, End);
 }
 
 /* Sends a connection response to a new node. Generates a random nodeID
 and sends that ID in the destID field */
 fsm request_response {
-	srand((unsigned) time(&t));
-	byte newID = (byte) ((rand() % 254) + 1); // Number 1-254
-	struct msg * payload;
-	payload = msg_init(newID, nodeID, 1, hopCount, powerLVL);
+	// time_t t;
+	// srand((unsigned) time(&t));
+	state Init:
+		byte newID = (byte) ((rand() % 254) + 1); // Number 1-254
+		struct msg * payload;
+		payload = msg_init(newID, nodeID, 1, hopCount, power);
 
 	state Sending:
 		packet = tcv_wnp(Sending, sfd, 12);
@@ -258,12 +93,10 @@ fsm request_response {
 fsm parent_send {
 	struct msg * payload;
 	address packet;
-	word ReadPower;
 
 	/* Initializes the msg packet */
 	state Init_t:
-		tcv_control (sfd, PHYSOPT_GETPOWER, &ReadPower);
-		payload = msg_init(destID, 0, hopCount, ReadPower);
+		payload = msg_init(destID, nodeID, 0, hopCount, power);
 
 	state Sending:
 		packet = tcv_wnp(Sending, sfd, 12);
@@ -354,7 +187,7 @@ fsm receive {
 		}
 		/* If the message comes from the parent node. */
 		if(payload->nodeID == parentID) {
-			proceed FromParent
+			proceed FromParent;
 		}
 
 		/* If the message comes from a child node. */
@@ -396,7 +229,7 @@ fsm receive {
 		ufree(payload);
 		proceed Receiving;
 
-	state FromParentInit:
+	state FromParent:
 		ledOn++;
 		leds(0, ledOn);
 		for (i = 0; i < ((sizeof(child_array)/sizeof(byte))); i++) {
@@ -435,6 +268,171 @@ fsm receive {
 		}
 		// If this is a totally new unknown node, send connection response.
 		else {
-			call request_response;
+			call request_response(Receiving);
 		}
+}
+
+// Main fsm
+fsm root {
+	struct msg * payload;
+	lword t;
+	lword timeout;
+	address packet;
+	word p1, tr;
+	byte RSSI, LQI;
+	word ReadPower;
+	int count = 0;
+	char c;
+
+	/* initializes the root */
+	state Init:
+		phys_cc1100(0, CC1100_BUF_SZ);
+		tcv_plug(0, &plug_null);
+		sfd = tcv_open(NONE, 0, 0);
+		if (sfd < 0) {
+			diag("unable to open TCV session.");
+			syserror(EASSERT, "no session");
+		}
+		tcv_control(sfd, PHYSOPT_ON, NULL);
+
+	/* Initializes the msg packet */
+	state Init_t:
+		tcv_control (sfd, PHYSOPT_SETPOWER, &power);
+		tcv_control (sfd, PHYSOPT_GETPOWER, &ReadPower);		
+		leds(1, 1);
+
+	/* Ask if this node will be the sink */
+	state ASK_ser:
+		timeout = seconds();
+		ser_outf(ASK_ser, "Would you like to make this the sink node? (y/n)\n\r");
+
+	/* Wait for response */
+	state WAIT_ser:
+
+		if(((seconds())-timeout)>30){proceed Sending;}
+		ser_inf(WAIT_ser, "%c", &c);
+       	/* Sets the sink nodeID to be 0. */
+		if(c == 'y') {
+			nodeID = 0;
+			parentID = 0;
+			hopCount = 0;
+			// Run the receive fsm and don't try to connect to the tree
+			call receive(End);
+		}
+		//payload->nodeID = nodeID;
+
+	// Sends a request to join the tree if not sink node
+	state Sending:
+		payload = msg_init(-1, nodeID, 1, 0, ReadPower);
+		//Sets timer
+		t = seconds();
+		packet = tcv_wnp(Sending, sfd, 12);
+		packet[0] = 0;
+
+		char * p = (char *)(packet+1);
+		*p = payload->nodeID;p++;
+		*p = payload->destID;p++;
+		*p = payload->sourceID;p++;
+		*p = payload->connect;p++;
+		*p = payload->hopCount;p++;
+		*p = payload->powerLVL;
+
+		tcv_endp(packet);
+		ufree(payload);
+
+	// Wait to receive a response from a node in the tree
+	state Wait_Connection:
+		// At end of 1.5 seconds, check if the node received a connection
+		if((seconds()-t) > 1.5){
+			// If connection end
+			if(count >= 1){
+				call receive(Connected);
+			}
+			//else power up
+            proceed Power_Up;
+        }
+		// RSSI is checked by potential parents
+		packet = tcv_rnp(Wait_Connection, sfd);
+
+	// Get information from received packet
+	state Measuring:
+		p1 = (tcv_left(packet))>>1;
+		tr = packet[p1-1];
+		RSSI = (byte) (tr>>8);
+		LQI = (byte) tr;
+
+	// Compare the responding nodes and only save the best one
+	state Update:
+		struct msg* payload = (struct msg*)(packet + 1);
+		if (payload->connect == 1) {
+			// Checks for multiple responses
+			count ++;
+			if(count > 1){
+				leds_all(0);
+				leds(2, 1);
+				// If the newly received response is further away than a previous response
+				if((payload->hopCount + 1) > hopCount){
+					proceed Wait_Connection;
+				}
+			}
+			// Updates if better connection
+			RSSI_C = RSSI;
+			LQI_C = LQI;
+			hopCount = payload->hopCount + 1;
+			/* parentID is set to the nodeID of the message
+			So the node id will always be id of the node who
+			sent the message */
+			parentID = payload->nodeID;
+			nodeID = payload->destID;
+		}
+
+		tcv_endp(packet);
+		// Go back to listening for connections until 1.5 seconds passes
+		proceed Wait_Connection;
+
+	// Increments power until max and if max sends to shutdown.
+	state Power_Up:
+		if(power == 7){
+			proceed Shut_Down;
+		}
+		power++;
+		tcv_control (sfd, PHYSOPT_SETPOWER,(&power));
+		proceed Sending;
+
+	// If no suitable nodes respond, shut down.
+	state Shut_Down:
+		leds_all(0);
+		finish;
+
+	// Generates final connection message
+	state Prep_Message:
+		payload = (struct msg *)umalloc(sizeof(struct msg));
+		payload->nodeID = nodeID;
+       		payload->destID = destID;
+		payload->connect = 1;
+		payload->hopCount = hopCount;
+		payload->powerLVL = (byte) ReadPower;
+
+	// Informs parent that it now has a child
+	state Connected:
+		payload = msg_init(destID, nodeID, 1, hopCount, ReadPower);
+		packet = tcv_wnp(Connected, sfd, 12);
+		packet[0] = 0;
+
+		char * p = (char *)(packet+1);
+		*p = payload->nodeID;p++;
+		*p = payload->destID;p++;
+		*p = payload->sourceID;p++;
+		*p = payload->connect;p++;
+		*p = payload->hopCount;p++;
+		*p = payload->powerLVL;
+
+		tcv_endp(packet);
+		ufree(payload);
+		call receive(End);
+		
+	// If root finishes the whole program stops. Keep root running.
+	// TODO: Is there a better way for root to continue infinitely? This will end eventually
+	state End:
+		delay(10000000, End);
 }
